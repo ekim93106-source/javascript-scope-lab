74f4bb226e3742be740ec374c5a5c07f74d9dd43
kimani <ekim93106@gmail.com>
2025-12-03 15:33:48 +0300

    completed scope

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "strawberryMilkshake";
+
+function addBurger() {
+    let newBurger = "Flatburger";
+    burgers.push(newBurger);
+}
+
+if (true) {
+    let anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+}
+function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake";
+}
+
+console.log(burgers);
+console.log(featuredDrink);
+addBurger();
+changeFeaturedDrink();
+console.log(burgers);
+console.log(featuredDrink);
