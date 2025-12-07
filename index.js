a74799021931bfb0d57bbfdbd5bc8713d3315035
kimani <ekim93106@gmail.com>
2025-12-07 12:13:41 +0300

    fixing the errors

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let featuredDrink = "Strawberry Milkshake";
 
 function addBurger() {
     const newBurger = "Flatburger";
-    burgers.push(newBurger);
-
-    if (true) {
-        const anotherNewBurger = "Maple Bacon Burger";
-        burgers.push(anotherNewBurger);
-    }
+    burgers.push(newBurger);   
+}
+if(true) {
+  const anotherNewBurger = "Maple Bacon Burger";
+  burgers.push(anotherNewBurger);
 }
 
 function changeFeaturedDrink() {
     featuredDrink = "The JavaShake";
 }
+ 
